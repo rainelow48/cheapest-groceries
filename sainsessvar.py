7b2f3da5beab054cf63c57b47ea995ca6f93b6fc
unknown <rainelow48@gmail.com>
2020-12-17 14:41:43 +0000

    added file to contain all sains essentials variables

---
+++ sainsessvar.py
@@ -0,0 +1,68 @@
+# This file contains variables relevant to Sainsburys Essentials
+
+# Dictionary of categories
+categories = {
+    1 : 'Fruits and Vegetables',
+    2 : 'Meat and Fish',
+    3 : 'Dairy, eggs and Chilled',
+    4 : 'Bakery',
+    5 : 'Frozen',
+    6 : 'Food Cupboard',
+    7 : 'Household'
+    # 8 : 'Drinks'
+}
+
+# Dictionary of filenames
+fileNames = {
+    'Fruits and Vegetables': 'fruitandveg',
+    'Meat and Fish': 'meatandfish',
+    'Dairy, eggs and Chilled': 'dairyeggsandchilled',
+    'Bakery': 'bakery',
+    'Frozen': 'frozen',
+    'Food Cupboard': 'foodcupboard',
+    'Household': 'household',
+    'Drinks': 'drinks'
+}
+
+# URL to be scrapped
+mainurl = 'https://www.sainsburys.co.uk/shop/CategoryDisplay'
+
+# URL parameters
+urlParams = {
+    'listId': '',
+    'catalogID': '10241',
+    'searchTerm': '',
+    'beginIndex': '',  # To adjust to get next page of items
+    'pageSize': '120',
+    'orderBy': 'TOP_SELLERS|SEQUENCING',
+    'top_category': '', # To input from parentCatIDs
+    'langId': '44',
+    'storeId': '10151',
+    'categoryId': '',   # To input from categoryIDs
+    'promotionId': '',
+    'parent_category_rn': '' # To input from parentCatIDs
+}
+
+# Category IDs
+categoryIDs = {
+    'Fruits and Vegetables': '474593',
+    'Meat and Fish': '474595',
+    'Dairy, eggs and Chilled': '474592',
+    'Bakery': '474594',
+    'Frozen': '474596',
+    'Food Cupboard': '442361',
+    'Household': '478352',
+    'Drinks': '' #drinks has no essentials
+}
+
+# Parent Category IDs
+parentCatIDs = {
+    'Fruits and Vegetables': '12518',
+    'Meat and Fish': '13343',
+    'Dairy, eggs and Chilled': '428866',
+    'Bakery': '12320',
+    'Frozen': '218831',
+    'Food Cupboard': '12422',
+    'Household': '12564',
+    'Drinks': '12192'
+}
