22fa22a90712e76d36bf597771ad58d11bb21847
unknown <rainelow48@gmail.com>
2020-12-20 19:16:37 +0000

    tidied up code

--- main.py
+++ main.py
@@ -1,56 +1,28 @@
 # Import required file function
-from userinteraction import searchDomain, searchCat, updatefile, oldcsv
+from userinteraction import searchDomain, searchCat, updatefile, oldcsv, printResult
 from updatecsv import setVar
 from searchcsv import searchItem, sortItems
 
-# Import library
-import time
+def main():
+    print("\n*** Welcome to Sainsbury's search function! ***\n")    # Welcome message
 
-# Welcome message
-print("\n*** Welcome to Sainsbury's search function! ***\n")
+    searchall = True                                                # Sets search domain to all items, 
+    # searchall = searchDomain()                                    # Use to obtain user's preferred search domain
+    categories, fileNames, mainurl, urlParams = setVar(searchall)   # Set correct variables
+    cat = searchCat(categories)                                     # Obtain user's preferred search category
 
-# Obtain user's preferred search domain
-# searchall = searchDomain()
-searchall = True # Sets search domain to all items
-
-# Set correct categories and filenames
-categories, fileNames, mainurl, urlParams = setVar(searchall)
-
-# Obtain user's preferred search category
-cat = searchCat(categories)
-
-# Checks if csv file is older than ## days, update if required
-if (oldcsv(categories, fileNames, cat, 1) == True): # 0 days: last updated today 
-    
-    # Time duration to update file, Takes 659 seconds to update all files
-    start_time = time.time()
-    updatefile(searchall, cat)
-    print("---- %.2f seconds ----" % (time.time()-start_time))
-else:
-    pass
-
-# Obtain search content from user
-query = input("\nPlease enter item to search for: ")
-searchValues = query.lower().split()
-
-# Search for item
-searchResult = searchItem(cat, categories, fileNames, searchValues)
-
-# Sort result
-result = sortItems(searchResult)
-
-# Prints result to user
-if (len(result) == 0):
-    print("No items were found")
-
-else:
-    if (cat == 0):
-        print("Found ", len(result), "items in All")
+    # Checks if csv file is older than ## days, prompt update if required
+    days = 0                                                        # To be set by user, "days = 0" to trigger update prompt
+    if (oldcsv(categories, fileNames, cat, days) == True): 
+        updatefile(searchall, cat)
     else:
-        print("Found ", len(result), "items in", categories[cat])
-    
-    # If result is <100 items, print all to user, else, print shortened table
-    if (len(result) > 100):
-        print(result)
-    else:
-        print(result.to_string())
+        pass
+
+    searchValues = input("\nPlease enter item to search for: ").lower().split()     # Obtain search content from user
+    searchResult = searchItem(cat, categories, fileNames, searchValues)             # Search for item
+    result = sortItems(searchResult)                                                # Sort result
+    length = 150                                                                    # Set cut off length for results table
+    printResult(result, cat, categories, length)                                    # Prints result to user
+
+if __name__ == "__main__":
+    main()
